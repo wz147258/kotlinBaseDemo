ce64cc6abadc5fce3c1b76915e4f360cec3a2745
wangzheng <wangzheng.20210419@bytedance.com>
2023-10-11 13:51:34 +0800

    1

--- app/src/main/java/com/dawn/kotlinbasedemo/MainActivity.kt
+++ app/src/main/java/com/dawn/kotlinbasedemo/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.dawn.kotlinbasedemo
 
 import android.os.Bundle
+import androidx.lifecycle.flowWithLifecycle
 import com.dawn.kotlinbasedemo.databinding.ActivityMainBinding
 import com.dawn.kotlinbasedemo.vm.MainVm
 import com.dawn.lib_base.base.BaseActivity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.launch
 
-
-class MainActivity : BaseActivity<ActivityMainBinding, MainVm>(){
+class MainActivity : BaseActivity<ActivityMainBinding, MainVm>() {
     override fun getLayoutId(): Int {
-       return R.layout.activity_main
+        return R.layout.activity_main
     }
 
     override fun initData(savedInstanceState: Bundle?) {
@@ -16,14 +23,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainVm>(){
         vm.requestFlowData()
 //        viewModel.requestLoopData();
 //        viewModel.requestWithTake()
+//        suspendCancellableCoroutine {
+//
+//        }
+        MainScope().launch {
+            flow<String> {
 
+            }.flowOn(Dispatchers.IO)
+//                .stateIn()
+//                .shareIn()
+                .flowWithLifecycle(lifecycle)
+                .collect {
 
+                }
+        }
     }
 
     override fun getVariableId(): Int {
         return BR.mainVm
 
     }
-
-
 }
